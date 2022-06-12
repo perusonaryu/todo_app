ab0acb59e88a7c9d30b061465308667a7d29ed3e
ryusei <perusonaryu1127@gmail.com>
2022-06-12 22:57:51 +0900

    Task 一覧APIの実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,23 +9,12 @@ use App\Models\Task;
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * タスク一覧
+     * @return \Illuminate\Support\Collection
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Task::orderByDesc('id')->get();
     }
 
     /**
@@ -46,17 +35,6 @@ class TaskController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
     {
         //
     }
